c397d8138e7fc95834f99b45f806e57313d3cc07
suguru.imanaga <suguru.imanaga@ctc-g.co.jp>
2020-04-03 17:07:02 +0900

    Add @SessionAttributes

--- src/main/java/jp/co/ocp/sample/spring_session/controller/WelcomeController.java
+++ src/main/java/jp/co/ocp/sample/spring_session/controller/WelcomeController.java
@@ -4,9 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
+@SessionAttributes(types = WelcomeForm.class)
 public class WelcomeController {
 
 	@ModelAttribute
