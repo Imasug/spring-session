2e486ab347dfe248e67c26180147b94fcdde1bc8
suguru.imanaga <suguru.imanaga@ctc-g.co.jp>
2020-04-06 12:21:17 +0900

    Make it possible to get a redis password through a environment variable

--- src/main/java/jp/co/ocp/sample/spring_session/config/SessionConfig.java
+++ src/main/java/jp/co/ocp/sample/spring_session/config/SessionConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+import org.springframework.util.StringUtils;
 
 /**
  * @see <a href="https://docs.spring.io/spring-data/data-redis/docs/2.2.5.RELEASE/reference/html/#reference">Spring Data Redis</a>
@@ -22,15 +23,18 @@ public class SessionConfig {
 	@Value("${REDIS_SERVICE_PORT:6379}")
 	private int port;
 
+	@Value("${database-password:}")
+	private String password;
+
 	@Bean
 	public LettuceConnectionFactory connectionFactory() {
-		System.out.println(hostName);
-		System.out.println(port);
+
+		System.out.println(String.format("hostName: %s, port: %s, password: %s", hostName, port, password));
+
 		RedisStandaloneConfiguration config = new RedisStandaloneConfiguration(hostName, port);
 
-		// TODO
-		if (!"localhost".equals(hostName)) {
-			config.setPassword("pLRI8y0PVuKtT7wX");
+		if (!StringUtils.isEmpty(password)) {
+			config.setPassword(password);
 		}
 
 		return new LettuceConnectionFactory(config);
