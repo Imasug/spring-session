552e63d4a93f6473824cf30a3a46d75fa46e7b69
suguru.imanaga <suguru.imanaga@ctc-g.co.jp>
2020-04-30 10:08:57 +0900

    Fix dependencies

--- src/main/java/jp/co/ocp/sample/spring_session/config/SessionConfig.java
+++ src/main/java/jp/co/ocp/sample/spring_session/config/SessionConfig.java
@@ -1,12 +1,14 @@
 package jp.co.ocp.sample.spring_session.config;
 
-import org.springframework.beans.factory.annotation.Value;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.core.env.Environment;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
-import org.springframework.util.StringUtils;
+import org.springframework.session.web.context.AbstractHttpSessionApplicationInitializer;
 
 /**
  * @see <a href="https://docs.spring.io/spring-data/data-redis/docs/2.2.5.RELEASE/reference/html/#reference">Spring Data Redis</a>
@@ -15,28 +17,31 @@ import org.springframework.util.StringUtils;
  */
 @Configuration
 @EnableRedisHttpSession
-public class SessionConfig {
+public class SessionConfig extends AbstractHttpSessionApplicationInitializer {
 
-	@Value("${REDIS_SERVICE_HOST:localhost}")
-	private String hostName;
+	Logger log = LoggerFactory.getLogger(this.getClass());
 
-	@Value("${REDIS_SERVICE_PORT:6379}")
-	private int port;
-
-	@Value("${database-password:}")
-	private String password;
+	@Autowired
+	Environment env;
 
 	@Bean
 	public LettuceConnectionFactory connectionFactory() {
 
-		System.out.println(String.format("hostName: %s, port: %s, password: %s", hostName, port, password));
+		String redisBaseEnvName = env.getProperty("REDIS_APPLICATION_NAME", "").toUpperCase().replace("-", "_");
+		String redisHostEnvName = String.format("%s_SERVICE_HOST", redisBaseEnvName);
+		String redisPortEnvName = String.format("%s_SERVICE_PORT", redisBaseEnvName);
 
-		RedisStandaloneConfiguration config = new RedisStandaloneConfiguration(hostName, port);
+		String redisHost = env.getProperty(redisHostEnvName, "localhost");
+		Integer redisPort = env.getProperty(redisPortEnvName, Integer.class, 6379);
+		String redisPassword = env.getProperty("REDIS_PASSWORD", "");
 
-		if (!StringUtils.isEmpty(password)) {
-			config.setPassword(password);
-		}
+		log.debug("redisHost: {}, redisPort: {}, redisPassword: {}", redisHost, redisPort, redisPassword);
 
-		return new LettuceConnectionFactory(config);
+		LettuceConnectionFactory factory = new LettuceConnectionFactory();
+		factory.setHostName(redisHost);
+		factory.setPort(redisPort);
+		factory.setPassword(redisPassword);
+
+		return factory;
 	}
 }
